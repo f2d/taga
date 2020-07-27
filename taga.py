112406d97451cf6ae02ba26568153dc186153d87
f2d <from2d@gmail.com>
2020-07-27 20:10:00 +0300

    Update for rewritten discord.py

--- taga.py
+++ taga.py
@@ -1,30 +1,42 @@
+
 import logging
 import discord
 from discord.ext.commands import Bot
 import tagaEvents
 
 class Taga:
-    def __init__(self, settings):
-        logging.basicConfig(level=logging.INFO)
-        logging.info('Nuclear launch detected!')
+	def __init__(self, settings):
+		logging.basicConfig(level=logging.INFO)
+		logging.info('Initializing settings.')
 
-        self.token = settings['token']
-        self.server_id = settings['server_id']
-        self.channel_id = settings['channel_id']
-        self.role_id = settings['role_id']
-        self.prefix = settings['command_prefix']
-        self.commands = settings['commands']
+		self.token	= settings['token']
+		self.prefix	= settings['command_prefix']
+		self.commands	= settings['commands']
+		self.server_id	= int(settings['server_id'])
+		self.channel_id	= int(settings['channel_id'])
+		self.role_id	= int(settings['role_id'])
 
-        logging.info('Logging in with token {}\nwith server_id {}\nwith role_id {}\nwith prefix {}\nand commands {}'
-                     .format(self.token, self.channel_id, self.role_id, self.prefix, self.commands))
+		logging.info('\n'.join([
+			'Logging in with token %s' % self.token
+		,	'with server_id %s' % self.channel_id
+		,	'with role_id %s' % self.role_id
+		,	'with prefix %s' % self.prefix
+		,	'and commands %s' % self.commands
+		]))
 
-        self.client = Bot(self.prefix)
+		self.client = Bot(self.prefix)
 
-        self.load_events()
+		self.load_events()
 
-    def load_events(self):
-        tagaEvents.import_events(self.client, self.prefix, self.commands, self.server_id, self.channel_id, self.role_id)
+	def load_events(self):
+		tagaEvents.import_events(
+			self.client
+		,	self.prefix
+		,	self.commands
+		,	self.server_id
+		,	self.channel_id
+		,	self.role_id
+		)
 
-    def run(self):
-        self.client.run(self.token)
-
+	def run(self):
+		self.client.run(self.token)
--- tagaEvents.py
+++ tagaEvents.py
@@ -1,37 +1,61 @@
+﻿
 import logging
-from discord.utils import get
-
+import discord
 
 def import_events(client, prefix, commands, server_id, channel_id, role_id):
 
-    @client.event
-    async def on_ready():
-        logging.info('Logged in.')
+	@client.event
+	async def on_ready():
+		logging.info('Logged in.')
 
-    @client.command(name=commands['marking'],
-                    pass_context=True)
-    async def mark(context):
-        server_match = True if context.message.server.id == server_id else False
-        channel_match = True if context.message.channel.id == channel_id else False
-        role_exists = True if role_id in [x.id for x in context.message.server.roles] else False
-        user_has_role = True if role_id in [x.id for x in context.message.author.roles] else False
-        role = get(context.message.server.roles, id=role_id)
+	@client.command(name=commands['barking']['name'], pass_context=True)
+	async def bark(context, arg=None):
+		try:
+			num = int(arg)
+			if num < 1: num = 1
+			if num > 9: num = 9
+		except Exception:
+			num = 1
 
-        if server_match and channel_match:
-            if role_exists:
-                if not user_has_role:
-                    await client.add_roles(context.message.author, role)
-                    await client.say('Я тебя запомнила, {}'.format(context.message.author.mention))
-                    logging.info('User id={} was given specified role.'.format(context.message.author.id))
-                else:
-                    await client.say('Ты уже был отмечен, {}'.format(context.message.author.mention))
-                    logging.warning('User id={} has been already given role id={}'.format(context.message.author.id,
-                                                                                          role_id))
-            else:
-                await client.say('Нет такой роли с id={}!'. format(role_id))
-                logging.error('No role with id={}'.format(role_id))
-        else:
-            logging.error('Server or channel does not match!\n{}/{}\n{}/{}'.format(context.message.server.id,
-                                                                                     server_id,
-                                                                                     context.message.channel.id,
-                                                                                     channel_id))
+		logging.info('Woof x %s from message "%r".' % (num, context.message))
+
+		await context.send('Woof!' * num)
+
+	@client.command(name=commands['marking']['name'], aliases=commands['marking']['aliases'], pass_context=True)
+	async def mark(context):
+		server_match  = (context.message.guild.id   == server_id)
+		channel_match = (context.message.channel.id == channel_id)
+		role_exists   = (role_id in [x.id for x in context.message.guild.roles])
+		user_has_role = (role_id in [x.id for x in context.message.author.roles])
+		role_to_add   = discord.utils.get(context.message.guild.roles, id=role_id)
+
+		if server_match and channel_match:
+			if role_exists:
+				if not user_has_role:
+					await context.message.author.add_roles(role_to_add)
+					await context.send('Я тебя запомнила, {}'.format(
+						context.message.author.mention
+					))
+
+					logging.info('User id={} was given specified role.'.format(
+						context.message.author.id
+					))
+				else:
+					await context.send('Ты уже был отмечен, {}'.format(
+						context.message.author.mention
+					))
+
+					logging.warning('User id={} has been already given role id={}'.format(
+						context.message.author.id
+					,	role_id
+					))
+			else:
+				await context.send('Нет такой роли с id={}!'.format(role_id))
+				logging.error('No role with id={}'.format(role_id))
+		else:
+			logging.error('Server or channel does not match!\n{}/{}\n{}/{}'.format(
+				context.message.guild.id
+			,	server_id
+			,	context.message.channel.id
+			,	channel_id
+			))
